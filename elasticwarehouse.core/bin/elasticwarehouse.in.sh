0dc43701da940ad123822e9fd8f6cba4085548f9
EffiSoft <contact@elasticwarehouse.org>
2015-12-16 23:15:03 +0100

    migration to ES2.x

--- elasticwarehouse.core/bin/elasticwarehouse.in.sh
+++ elasticwarehouse.core/bin/elasticwarehouse.in.sh
@@ -2,7 +2,7 @@
 
 #ES_CLASSPATH=$ES_CLASSPATH:$EW_HOME/target/*:$EW_HOME/lib/*:$EW_HOME/lib/*:$EW_HOME/lib/sigar/*
 #ES_CLASSPATH=.:$EW_HOME/target/*:$EW_HOME/lib/*
-ES_CLASSPATH=.:$EW_HOME/lib/elasticwarehouse.core-1.2.2-2.1.0-jar-with-dependencies.jar:$EW_HOME/lib/*
+ES_CLASSPATH=.:$EW_HOME/lib/*
 
 if [ "x$ES_MIN_MEM" = "x" ]; then
     ES_MIN_MEM=256m
